446d2858d9ba325795c257e12753c7da2d87f049
Jonathan <60202293+AkkozaDevelops@users.noreply.github.com>
2020-07-18 23:38:20 -0400

    inital commit
    pog
    includes requiring the first few npm modules + a processor for unhandled rejections

---
+++ index.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var https = require('https');
+
+process.on('unhandledRejection', (error, promise) => {
+    console.log(' Oh Lord! We forgot to handle a promise rejection here: ', promise);
+    console.log(' The error was: ', error);
+});
